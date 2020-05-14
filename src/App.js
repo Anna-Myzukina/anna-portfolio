696703f4a56c11828cc79d4750418e79bdcb6ace
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-05-14 15:42:10 +0300

    Add portfolio part

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ function App() {
             <a className="nav-link js-scroll-trigger" href="#services">Projects</a>
           </li>
           <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#portfolio">About</a>
+          </li>
+          <li className="nav-item">
             <a className="nav-link js-scroll-trigger" href="#portfolio">Contact</a>
           </li>
         </ul>
@@ -33,6 +36,47 @@ function App() {
       </div>
     </div>
   </header>
+
+  <Portfolio portfolioLinks={portfolioLinks}></Portfolio>
+
+  <footer className="footer">
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col-md-4">
+          <span className="copyright">Copyright &copy; Your Website 2019</span>
+        </div>
+        <div className="col-md-4">
+          <ul className="list-inline social-buttons">
+            <li className="list-inline-item">
+              <a href="https://twitter.com/AnnaMuzykina" target="_blank">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="https://www.facebook.com/AnnMuzykina" target="_blank">
+                <i className="fa fa-facebook-f"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="https://www.linkedin.com/in/anna-muzykina/" target="_blank">
+                <i className="fa fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-md-4">
+          <ul className="list-inline quicklinks">
+            <li className="list-inline-item">
+              <a href="#something" target="_blank">Privacy Policy</a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#something" target="_blank">Terms of Use</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
     </div>
   );
 }
